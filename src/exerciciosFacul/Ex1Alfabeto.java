1de759eb61ee1f4b219b79e1351475b84c778592
Ketile Santos <65567655+ketilesantos@users.noreply.github.com>
2020-10-19 15:06:19 -0300

    Rename Alfabeto.java to Ex1Alfabeto.java

---
+++ src/exerciciosFacul/Ex1Alfabeto.java
@@ -0,0 +1,14 @@
+package exerciciosFacul;
+
+
+public class Alfabeto {
+	public static void main(String[] args) {
+		char[] alfabeto = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+		int posicao = 4;
+		int indice = posicao - 1;
+		System.out.println("Qual índice e valor da posição 4? " + "O valor da posição é " +  alfabeto[3] +
+				" e seu índice é " + indice);
+		System.out.println("Quantas posições tem o array? " + "O array tem "+alfabeto.length + " posições.");
+		
+	}
+}
