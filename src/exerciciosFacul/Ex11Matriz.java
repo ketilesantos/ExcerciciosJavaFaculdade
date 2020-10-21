8bfa2d0d6730611abfe4a107aceea7786afa0bce
Ketile Santos <65567655+ketilesantos@users.noreply.github.com>
2020-10-20 23:40:26 -0300

    commit

--- src/exerciciosFacul/Ex11Matriz.java
+++ src/exerciciosFacul/Ex11Matriz.java
@@ -1,28 +1,49 @@
 package exerciciosFacul;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex11Matriz {
+	private static double[][] matriz = new double[2][2];
 	public static void main(String[] args) {
+
+		double determinante = calculaMatriz(matriz);
 		
-		double[][] matriz = new double[2][2];
-		calculaMatriz(matriz);
+		System.out.println("O calculo do determinante é: " + determinante);
 		
+		for(double[] mat: matriz ) {
+			System.out.println("Impressão da matriz: " + Arrays.toString(mat));
+		}
 	}
 	
-	static double calculaMatriz(double [][] array) {
+	private static double calculaMatriz(double [][] array) {
 		Scanner entrada = new Scanner(System.in);
+		double variavel1 = 0;
+		double variavel12 = 0;
+		double variavel2 = 0;
+		double variavel21 = 0;
 		double det = 0;
 		for(int i = 0; i<array.length; i++) {
 			for (int j = 0; j < array.length; j++) {
-				System.out.println("Informe: ");
+				System.out.print("Informe: ");
 				array[i][j] = entrada.nextDouble();
-				det = array[j][j];
-				
+				if(i ==0 && j==0){
+                    variavel1=array[i][j];
+                }
+
+                if(i ==1 && j==1){
+                    variavel12=array[i][j];
+                }
+                
+                if(i ==0 && j==1){
+                    variavel2=array[i][j];
+                }
+
+                if(i ==1 && j==0){
+                    variavel21=array[i][j];
+                }
 			}
 		}
-		 System.out.println(det);
+	    det = (variavel1 *variavel12) - (variavel2 * variavel21);
 		entrada.close();
 		return det;
 	}
---
+++ src/exerciciosFacul/Ex17QuadradoMagico.java
@@ -0,0 +1,45 @@
+package exerciciosFacul;
+
+public class Ex17QuadradoMagico {
+	static int matriz[][] = new int[3][3];
+
+	public static void main(String[] args) {
+
+		matriz[0][0] = 2;
+		matriz[0][1] = 9;
+		matriz[0][2] = 4;
+		matriz[1][0] = 7;
+		matriz[1][1] = 5;
+		matriz[1][2] = 3;
+		matriz[2][0] = 6;
+		matriz[2][1] = 1;
+		matriz[2][2] = 8;
+
+		boolean verifica = eQuadrado(matriz);
+		System.out.println("Quadrado Magico: " + verifica);
+	}
+
+	private static boolean eQuadrado(int[][] matriz) {
+		boolean ehQuadradoMagico = true;
+		int primeiraLinha = 0;
+		int primeiraColuna = 0;
+
+		for (int i = 0; i < 3; i++) {
+			int totalLinha = 0;
+			int totalColuna = 0;
+			for (int j = 0; j < 3; j++) {
+				if (i == 0) {
+					primeiraLinha += matriz[i][j];
+					primeiraColuna += matriz[j][i];
+				}
+				totalLinha += matriz[i][j];
+				totalColuna += matriz[j][i];
+			}
+			if (totalLinha != primeiraLinha || totalLinha != totalColuna || totalColuna != primeiraColuna
+					|| primeiraColuna != primeiraLinha) {
+				ehQuadradoMagico = false;
+			}
+		}
+		return ehQuadradoMagico;
+	}
+}
